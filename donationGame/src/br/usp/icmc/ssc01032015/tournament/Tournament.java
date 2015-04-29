4b98ea41d6b444afd6155c2b8d39bf8f019e8daf
damascenodiego <Carlos.Damasceno08@gmail.com>
2015-04-29 15:07:22 -0300

    Tournament main class with reflection

---
+++ donationGame/src/br/usp/icmc/ssc01032015/tournament/Tournament.java
@@ -0,0 +1,79 @@
+package br.usp.icmc.ssc01032015.tournament;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import br.usp.icmc.ssc01032015.Competitor;
+
+
+public class Tournament {
+
+	private static Tournament instance;
+	
+	private Tournament() {
+	}
+	
+	public static Tournament getInstance(){
+		if (instance == null){
+			instance = new Tournament();
+		}
+		return instance;
+	}
+
+	private static void print(List<Competitor> competitors, int i) {
+		for (int j = 0; j < competitors.size() && j < i ; j++) {
+			System.out.println(competitors.get(j));
+		}
+
+	}
+
+	private static void encounter(Competitor one, Competitor two){
+		double donation = 0;
+		double donation2 = 0;
+
+		// get donation from one to two
+		donation = one.declareDonationTo(two);
+		two.informDonationFrom(one, donation);
+		// get donation from two to one
+		donation2 = two.declareDonationTo(one);
+		one.informDonationFrom(two, donation2);
+		// calculates donations
+		one.addCash(((10 - donation) + (donation2 * 2))/2);
+		two.addCash(((10 - donation2) + (donation * 2))/2);
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public void runTournament(int rounds, Map<Class,Integer> competitorSetup) {
+		// edit here with your classes
+		List<Competitor> competitors = new ArrayList<Competitor>();
+		try {
+			for (Class competitorClass : competitorSetup.keySet()) {
+				if (Competitor.class.isAssignableFrom(competitorClass)){
+					for (int i = 0; i < competitorSetup.get(competitorClass); i++) {
+						competitors.add((Competitor)competitorClass.newInstance());
+					}
+				}
+			}
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; i < rounds; i++) {
+			for (int j = 0; j < competitors.size(); j++) {
+				for (int j2 = j+1; j2 < competitors.size(); j2++) {
+					Competitor one = competitors.get(j);
+					Competitor two = competitors.get(j2);
+					encounter(one, two);
+				}
+			}
+		}
+		Collections.sort(competitors, (Competitor o1, Competitor o2) -> {
+			return Double.compare(o1.getCash(), o2.getCash());
+		});
+
+		print(competitors,competitors.size());
+	}
+}
---
+++ donationGame/src/br/usp/icmc/ssc01032015/tournament/Tournament0000000.java
@@ -0,0 +1,55 @@
+package br.usp.icmc.ssc01032015.tournament;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.usp.icmc.ssc01032015.competitors.CompetitorCalcMean;
+import br.usp.icmc.ssc01032015.competitors.CompetitorGimmeFive;
+import br.usp.icmc.ssc01032015.competitors.CompetitorGiveNothing;
+import br.usp.icmc.ssc01032015.competitors.CompetitorPlus10Percent;
+import br.usp.icmc.ssc01032015.competitors.CompetitorRandom;
+
+public class Tournament0000000 {
+
+	public static void main(String[] args) {
+		
+		run10round0compet();
+		run10rounds10competEach();
+		run10rounds1competEach();
+
+	}
+	
+	private static final void run10rounds10competEach() {
+		Map<Class,Integer> competitors = new HashMap<Class,Integer>();
+		int rounds = 10;
+		competitors.put(CompetitorCalcMean.class, 		10);
+		competitors.put(CompetitorGimmeFive.class, 		10);
+		competitors.put(CompetitorGiveNothing.class, 	10);
+		competitors.put(CompetitorPlus10Percent.class, 	10);
+		competitors.put(CompetitorRandom.class, 		10);
+		Tournament.getInstance().runTournament(rounds,competitors);
+	}
+
+	private static final void run10rounds1competEach() {
+		Map<Class,Integer> competitors = new HashMap<Class,Integer>();
+		int rounds = 10;
+		competitors.put(CompetitorCalcMean.class, 		1);
+		competitors.put(CompetitorGimmeFive.class, 		1);
+		competitors.put(CompetitorGiveNothing.class, 	1);
+		competitors.put(CompetitorPlus10Percent.class, 	1);
+		competitors.put(CompetitorRandom.class, 		1);
+		Tournament.getInstance().runTournament(rounds,competitors);
+	}
+
+	private static final void run10round0compet() {
+		Map<Class,Integer> competitors = new HashMap<Class,Integer>();
+		int rounds = 10;
+		competitors.put(CompetitorCalcMean.class, 		0);
+		competitors.put(CompetitorGimmeFive.class, 		0);
+		competitors.put(CompetitorGiveNothing.class, 	0);
+		competitors.put(CompetitorPlus10Percent.class, 	0);
+		competitors.put(CompetitorRandom.class, 		0);
+		Tournament.getInstance().runTournament(rounds,competitors);
+	}
+
+}
